1e6e126d8b0fb4d5934d01efd264389f05059a51
Jan Moeller <jan.moeller0@gmail.com>
2017-03-17 21:48:29 +0100

    Add hammerjs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule }            from '@angular/core';
 import { CommonModule }        from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { RectBtnComponent } from './rect-btn/rect-btn.component';
 
